81b6cde66fe03a33f7c8be99a1eaf3f036b0a632
Igor Diakov <igord@dcomplex.com>
2017-04-24 14:49:32 -0400

    Initial project template from Udacity

---
+++ Term2 Sensor Fusion, Localization, and Control/Project3 Kidnapped Vehicle/src/particle_filter.h
@@ -0,0 +1,117 @@
+/*
+ * particle_filter.h
+ *
+ * 2D particle filter class.
+ *  Created on: Dec 12, 2016
+ *      Author: Tiffany Huang
+ */
+
+#ifndef PARTICLE_FILTER_H_
+#define PARTICLE_FILTER_H_
+
+#include "helper_functions.h"
+
+struct Particle {
+
+	int id;
+	double x;
+	double y;
+	double theta;
+	double weight;
+    
+    LandmarkObs transform(const LandmarkObs obs);
+};
+
+class ParticleFilter {
+	// Number of particles to draw
+	const int num_particles;
+
+	// Flag, if filter is initialized
+	bool is_initialized = false;
+	
+	// Vector of weights of all particles
+	std::vector<double> weights;
+public:
+
+	// Set of current particles
+	std::vector<Particle> particles;
+
+	// Constructor
+	// @param M Number of particles
+	ParticleFilter() : num_particles(0) {}
+
+	// Destructor
+	~ParticleFilter() {}
+
+	/**
+	 * init Initializes particle filter by initializing particles to Gaussian
+	 *   distribution around first position and all the weights to 1.
+	 * @param x Initial x position [m] (simulated estimate from GPS)
+	 * @param y Initial y position [m]
+	 * @param theta Initial orientation [rad]
+	 * @param std[] Array of dimension 3 [standard deviation of x [m], standard deviation of y [m]
+	 *   standard deviation of yaw [rad]]
+	 */
+	void init(double x, double y, double theta, double std[]);
+
+	/**
+	 * prediction Predicts the state for the next time step
+	 *   using the process model.
+	 * @param delta_t Time between time step t and t+1 in measurements [s]
+	 * @param std_pos[] Array of dimension 3 [standard deviation of x [m], standard deviation of y [m]
+	 *   standard deviation of yaw [rad]]
+	 * @param velocity Velocity of car from t to t+1 [m/s]
+	 * @param yaw_rate Yaw rate of car from t to t+1 [rad/s]
+	 */
+	void prediction(double delta_t, double std_pos[], double velocity, double yaw_rate);
+	
+	/**
+	 * dataAssociation Finds which observations correspond to which landmarks (likely by using
+	 *   a nearest-neighbors data association).
+	 * @param predicted Vector of predicted landmark observations
+	 * @param observations Vector of landmark observations
+	 */
+	void dataAssociation(std::vector<LandmarkObs> predicted, std::vector<LandmarkObs>& observations);
+	
+	/**
+	 * updateWeights Updates the weights for each particle based on the likelihood of the 
+	 *   observed measurements. 
+	 * @param sensor_range Range [m] of sensor
+	 * @param std_landmark[] Array of dimension 2 [standard deviation of range [m],
+	 *   standard deviation of bearing [rad]]
+	 * @param observations Vector of landmark observations
+	 * @param map Map class containing map landmarks
+	 */
+	void updateWeights(double sensor_range, double std_landmark[], std::vector<LandmarkObs> observations,
+			Map map_landmarks);
+	
+	/**
+	 * resample Resamples from the updated set of particles to form
+	 *   the new set of particles.
+	 */
+	void resample();
+	
+	/*
+	 * write Writes particle positions to a file.
+	 * @param filename File to write particle positions to.
+	 */
+	void write(std::string filename);
+	
+	/**
+	 * initialized Returns whether particle filter is initialized yet or not.
+	 */
+	const bool initialized() const {
+		return is_initialized;
+	}
+
+    template <typename P1, typename P2>
+    static double weight(const P1 &p1, const P2 &p2, const double sigma_x, const double sigma_y, const double sigma_z) {
+        return weight(p1.x, p1.y, p2.x, p2.y, sigma_x, sigma_y, sigma_z);
+    }
+
+    static double weight(double x1, double y1, double x2, double y2, double sigma_x, double sigma_y, double sigma_z);
+};
+
+
+
+#endif /* PARTICLE_FILTER_H_ */
